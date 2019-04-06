b96c1fa7435f872ca35daa9f03c4f213a3292abd
Chenhua Lu <luchenhua.840603@gmail.com>
2019-04-07 01:35:49 +0800

    Add some more docker images.

--- update_docker_images.sh
+++ update_docker_images.sh
@@ -2,20 +2,27 @@
 
 # update docker images.
 docker pull alpine;
+docker pull elasticsearch;
+# docker pull gcr.io/etcd-development/etcd;
 docker pull golang;
 docker pull gradle;
+docker pull kibana;
+docker pull logstash;
 docker pull maven;
 docker pull mongo;
 docker pull mysql;
 docker pull nginx;
 docker pull openjdk;
 docker pull postgres;
-docker pull sonarqube;
+docker pull quay.io/coreos/etcd;
 docker pull redis;
 docker pull solr;
-docker pull zookeeper;
-docker pull quay.io/coreos/etcd;
+docker pull sonarqube;
+docker pull swaggerapi/swagger-editor;
+docker pull swaggerapi/swagger-generator;
+docker pull swaggerapi/swagger-ui;
 docker pull wurstmeister/kafka;
+docker pull zookeeper;
 
 # remove trash images.
 docker rmi $(docker images | awk '$2~/<none>/{print $3}');
