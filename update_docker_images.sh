08cc651a7449a8c0039251a116afbc5873f8aa36
Chenhua Lu <luchenhua.840603@gmail.com>
2019-08-19 17:05:29 +0800

    <feat>: replace some alpine images with normal images for faster building purpose.

--- update_docker_images.sh
+++ update_docker_images.sh
@@ -3,37 +3,29 @@
 # update docker images.
 docker pull alpine:latest;
 
-docker pull nginx:alpine;
-
-# docker pull quay.io/coreos/etcd:latest;
 docker pull gcr.io/etcd-development/etcd:latest;
-docker pull consul:latest;
-docker pull zookeeper:latest;
+# docker pull consul:latest;
+# docker pull zookeeper:latest;
 
-docker pull golang:latest;
-docker pull golang:alpine;
-docker pull openjdk:latest;
-docker pull openjdk:alpine;
+docker pull golang:1.12;
+docker pull maven:3.6-jdk-11;
 
-docker pull gradle:alpine;
-docker pull maven:alpine;
+docker pull gcr.io/distroless/static:latest;
+docker pull gcr.io/distroless/base:latest;
+docker pull gcr.io/distroless/java:11;
 
-docker pull postgres:alpine;
-docker pull redis:alpine;
-docker pull mongo:latest;
-docker pull mysql:latest;
+docker pull postgres:11;
+docker pull redis:5;
+# docker pull mongo:latest;
+# docker pull mysql:latest;
 
-docker pull wurstmeister/kafka:latest;
+# docker pull wurstmeister/kafka:latest;
 
-# docker pull elasticsearch;
-# docker pull logstash;
-# docker pull kibana;
+# docker pull sonarqube:latest;
 
-docker pull sonarqube:latest;
-
-docker pull swaggerapi/swagger-editor:latest;
-docker pull swaggerapi/swagger-generator:latest;
-docker pull swaggerapi/swagger-ui:latest;
+# docker pull swaggerapi/swagger-editor:latest;
+# docker pull swaggerapi/swagger-generator:latest;
+# docker pull swaggerapi/swagger-ui:latest;
 
 # remove trash images.
 docker rmi $(docker images | awk '$2~/<none>/{print $3}');
