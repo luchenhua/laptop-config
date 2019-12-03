c1430181e9765daf158e381fae2ae90bca373181
Chenhua Lu <luchenhua.840603@gmail.com>
2019-12-03 10:52:55 +0800

    <feat>: update docker script.

--- update_docker_images.sh
+++ update_docker_images.sh
@@ -5,9 +5,8 @@
 docker pull alpine:latest;
 docker pull gcr.io/distroless/static:latest;
 docker pull gcr.io/distroless/base:latest;
-docker pull gcr.io/distroless/java:latest;
 docker pull gcr.io/distroless/java:11;
-docker pull gcr.io/distroless/java:8;
+# docker pull gcr.io/distroless/java:8;
 
 # - service discovery -
 docker pull gcr.io/etcd-development/etcd:latest;
@@ -15,29 +14,24 @@ docker pull gcr.io/etcd-development/etcd:latest;
 # docker pull zookeeper:latest;
 
 # - programming language compiler
-docker pull golang:latest;
 docker pull golang:1.13;
-docker pull maven:latest;
 docker pull maven:3.6-jdk-11;
 
 # - database -
-docker pull postgres:latest;
 docker pull postgres:11;
 docker pull postgres:9;
-docker pull redis:latest;
 docker pull redis:5;
-docker pull mongo:latest;
-docker pull mysql:latest;
+# docker pull mongo:latest;
+# docker pull mysql:latest;
 
 # - message queue -
-# docker pull wurstmeister/kafka:latest;
+docker pull wurstmeister/kafka:latest;
 
 # - source code check -
-# docker pull sonarqube:latest;
+docker pull sonarqube:latest;
 
 # - simple database management -
 docker pull adminer:latest;
-docker pull adminer:4;
 
 # - swagger api documentation -
 docker pull swaggerapi/swagger-editor:latest;
@@ -52,7 +46,7 @@ docker pull registry:latest;
 docker pull jaegertracing/all-in-one:latest;
 
 # - python data science -
-docker pull continuumio/anaconda3:latest;
+# docker pull continuumio/anaconda3:latest;
 
 
 # remove trash images.
