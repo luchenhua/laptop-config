ab218a8b558f474384323840c6f8e53c5b73460f
Chenhua Lu <luchenhua.840603@gmail.com>
2019-12-28 12:46:12 +0800

    Update script for docker.

--- update_docker_images.sh
+++ update_docker_images.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
 
 # download & update all the images
-# - base images -
+# --- base images ---
 # docker pull alpine:latest;
 docker pull gcr.io/distroless/static:latest;
 docker pull gcr.io/distroless/base:latest;
 docker pull gcr.io/distroless/java:11;
 
-# - service discovery -
+# --- service discovery ---
 docker pull gcr.io/etcd-development/etcd:latest;
 docker pull consul:latest;
 docker pull zookeeper:latest;
 
-# - programming language compiler
+# --- programming language compiler ---
 docker pull golang:1.13;
 docker pull maven:3.6-jdk-11;
 
-# - database -
+# --- database ---
 docker pull postgres:12;
 docker pull redis:5;
 docker pull mongo:4;
 docker pull mysql:8;
 
-# - message queue -
+# --- message queue ---
 docker pull bitnami/kafka:latest;
 
-# - source code check -
+# --- source code check ---
 docker pull sonarqube:latest;
 
-# - simple database management -
+# --- simple database management ---
 docker pull adminer:latest;
 
-# - swagger api documentation -
+# --- swagger api documentation ---
 docker pull swaggerapi/swagger-editor:latest;
 
-# - docker security check -
+# --- docker security check ---
 docker pull anchore/anchore-engine:latest;
 
-# - docker registry server -
+# --- docker registry server ---
 docker pull registry:latest;
 
-# - system tracing & monitoring -
+# --- system tracing & monitoring ---
 docker pull jaegertracing/all-in-one:latest;
 docker pull prom/prometheus:latest;
 docker pull grafana/grafana:latest;
@@ -50,7 +50,7 @@ docker pull docker.elastic.co/kibana/kibana:7.5.0;
 docker pull docker.elastic.co/logstash/logstash:7.5.0;
 docker pull docker.elastic.co/beats/filebeat:7.5.0;
 
-# - python data science -
+# --- python data science ---
 # docker pull continuumio/anaconda3:latest;
 
 # remove trash images.
