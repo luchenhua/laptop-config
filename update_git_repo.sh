6f9039ae4d3804cf321f89abfeb8f3713b8a01b8
Alex LU <alex.lu@decathlon.com>
2022-01-06 14:11:02 +0800

    <feat>: add new repo & reformat the struct.

--- update_git_repo.sh
+++ update_git_repo.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-cd ~/dev/repo/work/message-hub && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-adapter && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-api && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-auth && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-batch && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-callback && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-core && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-documentation && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-filter && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-fluent-bit && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-logging && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-logging-frontend && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-mock && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-poc && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-sender && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-subscription && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-sst && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/message-hub-template && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/msghub-be-subscription && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/msghub-be-template && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/msghub-be-wecom && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
+cd ~/dev/repo/work/message-hub && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-fluent-bit && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
 
-cd ~/dev/repo/work/msghub-doc && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
+cd ~/dev/repo/work/message-hub-adapter && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-api && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-auth && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-callback && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-core && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-filter && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-logging && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-sender && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-subscription && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-sst && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-template && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/msghub-be-subscription && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/msghub-be-template && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
 
-cd ~/dev/repo/work/k8s-mig-env && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/k8s-mig-validator && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/k8s-mig-kafka-consumer && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
-cd ~/dev/repo/work/k8s-mig-srv-go && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
+cd ~/dev/repo/work/message-hub-mock && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
 
-cd ~/dev/repo/work/K8S-APP-REPO-messageHub-FLUX && pwd && git fetch --all --prune && git checkout main && git pull && git checkout develop && git pull;
+cd ~/dev/repo/work/message-hub-poc && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-batch && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/msghub-be-wecom && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+
+cd ~/dev/repo/work/message-hub-logging-frontend && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/message-hub-template-editor && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+
+cd ~/dev/repo/work/message-hub-documentation && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/msghub-doc && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+
+cd ~/dev/repo/work/k8s-mig-env && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/k8s-mig-validator && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/k8s-mig-kafka-consumer && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/k8s-mig-srv-go && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+
+cd ~/dev/repo/work/K8S-APP-REPO-messageHub-FLUX && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+
+cd ~/dev/repo/work/wechat-ga-bigquery-export && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
+cd ~/dev/repo/work/lien_ftp && pwd && git fetch --all --prune && git switch main && git pull && git switch develop && git pull;
 
 cd ~;
